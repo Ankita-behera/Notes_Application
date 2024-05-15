e91318a31d8559819a9169c430b1c72e4df0cb9d
Ankita-behera <ankitab036@gmail.com>
2024-05-15 21:03:54 +0530

    added Notes App

---
+++ src/main/java/com/org/dao/NotesDao.java
@@ -0,0 +1,53 @@
+package com.org.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class NotesDao {
+	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("ankita");
+
+	public List<NotesDao> fetchNotesById(int id) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+
+        try {
+            Query query = em.createQuery("SELECT n FROM Note n WHERE n.id = :id");
+            query.setParameter("id", id);
+            List<NotesDao> notes = query.getResultList();
+            return notes;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        } finally {
+            em.close();
+        }
+    }
+	
+	public List<NotesDao> fetchAllNotes() {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+
+        try {
+            Query query = em.createQuery("SELECT n FROM Note n");
+            List<NotesDao> notes = query.getResultList();
+            return notes;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        } finally {
+            em.close();
+        }
+    }
+	
+	
+}
---
+++ src/main/java/com/org/dto/Notes.java
@@ -0,0 +1,26 @@
+package com.org.dto;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Getter
+@Setter
+public class Notes {
+	@Id
+  private int id;
+  private String title;
+  private String description;
+  
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn
+ private User users;
+  
+}
